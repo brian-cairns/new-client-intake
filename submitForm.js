7c7100f76dbcdba66fc7d265af3739470b752fde
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-08-01 06:06:48 -0400

    Update submitForm.js

--- submitForm.js
+++ submitForm.js
@@ -150,7 +150,7 @@ function showSuccess(formId) {
   document.getElementById('returnMessage').innerHTML = 'Form has been successfully submitted'
   printForm.style.display = 'inline';
   printForm.addEventListener('click', (e) => {
-  location.href = `https://phoenix-freedom-foundation-backend.webflow.io/completed-forms/new-client-intake-form?formId=${formId}`
+  location.href = `https://phoenix-freedom-foundation-backend.webflow.io/completed-forms/new-client-intake-form`
   })
 }
 
